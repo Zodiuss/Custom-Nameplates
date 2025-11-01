ea9d955cf4c2cd46dff7c365f0e165f218cc6873
Zodiuss <69404411+Zodiuss@users.noreply.github.com>
2025-11-01 14:36:58 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,10 +32,22 @@ subprojects {
     }
 }
 
-fun versionBanner() = project.providers.exec {
-    commandLine("git", "rev-parse", "--short=8", "HEAD")
-}.standardOutput.asText.map { it.trim() }.getOrElse("Unknown")
+fun versionBanner(): String {
+    return try {
+        project.providers.exec {
+            commandLine("git", "rev-parse", "--short=8", "HEAD")
+        }.standardOutput.asText.map { it.trim() }.getOrElse("Unknown")
+    } catch (e: Exception) {
+        "Unknown"
+    }
+}
 
-fun builder() = project.providers.exec {
-    commandLine("git", "config", "user.name")
-}.standardOutput.asText.map { it.trim() }.getOrElse("Unknown")
+fun builder(): String {
+    return try {
+        project.providers.exec {
+            commandLine("git", "config", "user.name")
+        }.standardOutput.asText.map { it.trim() }.getOrElse("Unknown")
+    } catch (e: Exception) {
+        "Unknown"
+    }
+}
